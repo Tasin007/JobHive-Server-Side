88f9a389babec433e12ab2202cf87fb5d5e744e6
Tasin007 <mdtasinalam.mta@gmail.com>
2023-11-09 18:16:42 +0600

    atlast all issues are complete

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000;
 // middleware
 // app.use(cors());
 app.use(cors({
-  origin: ["http://localhost:5173" , "https://job-hive-client-side.vercel.app/"],
+  origin: ["http://localhost:5173" , "https://job-hive-client-side.vercel.app"],
   credentials: true,
 }));
 
